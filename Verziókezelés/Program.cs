09b7856c41d8cbf19a3b62c4b7008c81c2a01eca
Petrik Lajos Szg <info@petrik.hu>
2020-09-21 14:03:34 +0200

    Számok bekérése v2

--- Verziókezelés/Program.cs
+++ Verziókezelés/Program.cs
@@ -7,16 +7,14 @@ namespace Verziókezelés
         static void Main(string[] args)
         {
             Console.WriteLine("Kérem adjon meg 10 számot: ");
-            int elsoszam = int.Parse(Console.ReadLine());
-            int masodikszam = int.Parse(Console.ReadLine());
-            int harmadikszam = int.Parse(Console.ReadLine());
-            int negyedikszam = int.Parse(Console.ReadLine());
-            int otodikszam = int.Parse(Console.ReadLine());
-            int hatodikszam = int.Parse(Console.ReadLine());
-            int hetedikszam = int.Parse(Console.ReadLine());
-            int nyolcadikszam = int.Parse(Console.ReadLine());
-            int kilencedikszam = int.Parse(Console.ReadLine());
-            int tizedikszam = int.Parse(Console.ReadLine());
+            int[] szamok = new int[10];
+            for (int i = 0; i < szamok.Length; i++)
+            {
+                Console.WriteLine("Adja meg a következő számot");
+                int beertek = int.Parse(Console.ReadLine());
+                szamok[i] = beertek;
+            }
+            
         }
     }
 }
